bb8cde3cd15b52dd9d42aada2c28e4fd4157ac85
Alberto <albertoc31@gmail.com>
2021-07-18 15:03:36 +0200

    Rentabilidad Calculator Service

---
+++ src/Service/RentabilidadCalculator.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Entity\Concierto;
+
+class RentabilidadCalculator
+{
+    public function calcula(Concierto $concierto)
+    {
+        $entrada = $concierto->getNumeroEspectadores() * $concierto->getRecinto()->getPrecioEntrada();
+        $costes_concierto = $concierto->getRecinto()->getCosteAlquiler();
+        $grupos = $concierto->getGrupos();
+
+        foreach ($grupos as $grupo)
+        {
+            $costes_concierto += $grupo->getCache();
+        }
+        $rentabilidad = $entrada - $costes_concierto;
+
+        $concierto->setRentabilidad($rentabilidad);
+        return $concierto;
+    }
+}
